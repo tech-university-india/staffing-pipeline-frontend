3d5e8281999965e1bfe6139ee74c3b2d8197cea1
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-03 15:21:19 +0530

    feat: add route and page for LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CaseStudiesPage from './pages/CaseStudiesPage';
+import EngagementDetailsPage from './pages/EngagementDetailsPage';
+import EngagementsPage from './pages/EngagementsPage';
+import LoginPage from './pages/LoginPage';
+import PeoplePage from './pages/PeoplePage';
+import PeopleDetailsPage from './pages/PeopleDetailsPage';
 
 import './App.css';
+import { LOGIN_ROUTE } from './constants/Routes';
 
 function App() {
-  return <div className="App">App</div>;
+  <BrowserRouter>
+    <Routes>
+      <Route path={LOGIN_ROUTE} element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
---
+++ src/constants/Routes/index.js
@@ -0,0 +1,7 @@
+export const LOGIN_ROUTE = '/login';
+export const ENGAGEMENTS_ROUTE = '/engagements';
+export const ENGAGEMENT_DETAILS_ROUTE = '/engagements/:engagementId';
+export const CASE_STUDIES_ROUTE = '/case-studies';
+export const PEOPLE_ROUTE = '/people';
+export const PEOPLE_DETAILS_ROUTE = '/people/:personId';
+export const ERROR_ROUTE = '/error';
