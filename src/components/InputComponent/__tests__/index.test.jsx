44978bbdb140d9800853205b73a548097a889485
Promit-revar <promit.revar2211@gmail.com>
2023-03-05 12:23:16 +0530

    Feat: Add test for specific rendering of InputComponent

--- src/components/InputComponent/__tests__/index.test.jsx
+++ src/components/InputComponent/__tests__/index.test.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import InputComponent from '..';
 
 describe('InputComponent', () => {
   it('should render correctly', () => {
-    const { asFragment } = render(
-        <InputComponent placeholder="email" />
-    );
+    const { asFragment } = render(<InputComponent placeholder="email" />);
     expect(asFragment()).toMatchSnapshot();
   });
+  it('should display placeholder value passed to it',()=>{
+    render(<InputComponent placeholder="email" />);
+    expect(screen.getByPlaceholderText('email')).toBeTruthy();
+  })
 });
