ea45153eba467ff0a9cd42053bfca38a7c198f73
Promit-revar <promit.revar2211@gmail.com>
2023-03-05 12:13:26 +0530

    feat: add input component

---
+++ src/components/InputComponent/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './InputComponent.css';
+import propTypes from 'prop-types'; 
+export default function InputComponent(props){
+  return (
+    <div>
+      <input type="text" className='text-input' placeholder={props.placeholder} />
+    </div>
+  );
+}
+InputComponent.propTypes = {    
+  placeholder: propTypes.string,
+};
