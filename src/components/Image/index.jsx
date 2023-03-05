08dde3da3302eb15c9a12724cd9c997444bf9ea2
Sneha <snehalall.sneha@gmail.com>
2023-03-05 13:58:43 +0530

    fix: fix border-radius in CSS

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './image.css';
 import PropTypes from 'prop-types';
 
-export default function Image({ imageUrl, altText, isACircle }) {
-  return <img src={imageUrl} alt={altText} className={`${isACircle ? 'circle-image' : ''}`} />;
+export default function Image({ imageUrl, altText }) {
+  return <img src={imageUrl} alt={altText} className="image-component" />;
 }
 
 Image.propTypes = {
   imageUrl: PropTypes.string.isRequired,
   altText: PropTypes.string.isRequired,
-  isACircle: PropTypes.bool,
 };
