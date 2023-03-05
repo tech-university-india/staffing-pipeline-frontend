26c3c8491b6fdadff4291fe0ee187457a505fba2
rishusingh022 <rishusingh022@gmail.com>
2023-03-05 12:28:06 +0530

    chore : add test to fetch specific user details when people details page is rendered

---
+++ src/pages/PeopleDetailsPage/__tests__/index.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PeopleDetailsPage from '..';
+import { waitFor, render } from '@testing-library/react';
+import { GET_USER_DATA_BY_ID_URL } from '../../../constants/apiEndpoints';
+import makeRequest from '../../../utils/makeRequest/';
+import { useParams } from 'react-router-dom';
+
+jest.mock('../../../utils/makeRequest');
+
+const mockNavigate = jest.fn();
+const mockUseParams = jest.fn();
+jest.mock('react-router-dom', () => ({
+  useNavigate: () => mockNavigate,
+  useParams: () => mockUseParams,
+}));
+
+describe('PeopleDetailsPage', () => {
+  it('should call the makeRequest function when people details page is rendered', async () => {
+    mockUseParams.mockReturnValue({ userId: '123' });
+    const { userId } = useParams();
+    makeRequest.mockResolvedValue({});
+    render(<PeopleDetailsPage />);
+    await waitFor(() => expect(makeRequest).toBeCalledWith(GET_USER_DATA_BY_ID_URL(userId), {}, mockNavigate));
+  });
+});
