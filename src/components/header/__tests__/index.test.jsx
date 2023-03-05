2a0020a41f08d67c4a36df4672054dab29442f70
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-03-05 11:14:37 +0530

    feat: add tests for header without navbar

--- src/components/header/__tests__/index.test.jsx
+++ src/components/header/__tests__/index.test.jsx
@@ -3,8 +3,12 @@ import { render } from '@testing-library/react';
 import Header from '..';
 
 describe('Header', () => {
-  it('should render correctly', () => {
+  it('should render correctly with a navbar when Navbar boolean is true', () => {
     const { asFragment } = render(<Header {...{ hasNav: true }} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it('should render correctly without a navbar when Navbar boolean is false', () => {
+    const { asFragment } = render(<Header {...{ hasNav: false }} />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
