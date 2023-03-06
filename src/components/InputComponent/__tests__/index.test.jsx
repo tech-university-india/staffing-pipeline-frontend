1a9b25b70afbff172fa777f666a27c0368f9c4d5
Promit-revar <promit.revar2211@gmail.com>
2023-03-06 22:16:07 +0530

    fix: new line issues

--- src/components/InputComponent/__tests__/index.test.jsx
+++ src/components/InputComponent/__tests__/index.test.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { render, screen } from '@testing-library/react';
+
 import InputComponent from '..';
 
 describe('InputComponent', () => {
@@ -7,6 +9,7 @@ describe('InputComponent', () => {
     const { asFragment } = render(<InputComponent placeholder="email" className="login-input" />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it('should display placeholder value passed to it', () => {
     render(<InputComponent placeholder="email" className="login-input" />);
     expect(screen.getByPlaceholderText('email')).toBeTruthy();
--- src/components/InputComponent/index.jsx
+++ src/components/InputComponent/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+
 import './InputComponent.css';
+
 import propTypes from 'prop-types';
-export default function InputComponent({ className, placeholder }) {
+
+export default function InputComponent({ placeholder }) {
   return (
     <div>
-      <input type="text" className={className} placeholder={placeholder} />
+      <input type="text" className={'login-input'} placeholder={placeholder} />
     </div>
   );
 }
+
 InputComponent.propTypes = {
   placeholder: propTypes.string,
-  className: propTypes.string.isRequired,
 };
