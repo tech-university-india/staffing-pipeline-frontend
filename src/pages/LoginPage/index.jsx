85a17f9e935033274deb5d164fd976356774a938
Promit-revar <promit.revar2211@gmail.com>
2023-03-12 11:18:16 +0530

    feat: add login page design

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import './LoginPage.css';
+import InputComponent from '../../components/InputComponent';
+import Footer from '../../components/footer';
+import Button from '../../components/Button';
 const LoginPage = () => {
+  const onSubmit = () => {
+    console.log('onSubmit');
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <div className='login-page'>
+        <div className={'card'}>
+          <h1>Hub Capablities and staffing portal</h1>
+          <InputComponent placeholder={'email'}/>
+          <InputComponent placeholder={'password'}/>
+          <Button buttonText={'login'} handleClick={onSubmit} />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
