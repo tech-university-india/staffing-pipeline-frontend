27fd50aa5740e4473d3646988b9305d5def38bcb
Harsh <harsh821agarwal@gmail.com>
2023-03-13 13:51:36 +0530

    chore: add snapshot test for ImageCard component

---
+++ src/components/ImageCard/__tests__/index.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ImageCard from '..';
+
+describe('ImageCard', () => {
+  it('should render correctly', () => {
+    const { asFragment } = render(
+      <ImageCard />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -14,18 +14,19 @@ export default function ImageCard({
   startDate,
   daysToGo,
   buttonText,
-  isUserCard
-}) {  
+  isUserCard,
+  handleButtonClick,
+}) {
   return (
-    <div className="shadow-lg flex flex-col w-56 image-style">
+    <div className="shadow-lg flex flex-col w-56 image-style" data-testid="image-card">
       <Image imageUrl={imageUrl} altText={altText} />
       <div className="px-3 py-2">
-        <div className="text-xs mb-1">{isUserCard? `FMNO: ${identityNumber}`: identityNumber}</div>
+        <div className="text-xs mb-1">{isUserCard ? `FMNO: ${identityNumber}` : identityNumber}</div>
         <div className="font-bold text-xl mb-1">{name}</div>
-        <div className="font-semibold text-xs mb-1">{isUserCard? designation: `Start Date: ${startDate}`}</div>
-        <div className="font-medium text-xs mb-1 text-gray-500">{isUserCard? location: `${daysToGo} Days to go`}</div>
+        <div className="font-semibold text-xs mb-1">{isUserCard ? designation : `Start Date: ${startDate}`}</div>
+        <div className="font-medium text-xs mb-1 text-gray-500">{isUserCard ? location : `${daysToGo} Days to go`}</div>
       </div>
-      <div className="link-button self-end text-xs mb-2 font-medium cursor-pointer">
+      <div className="link-button self-end text-xs mb-2 font-medium cursor-pointer" onClick={handleButtonClick}>
         {buttonText}
         <BsArrowRight className="inline-block mx-2 text-blue-800" />
       </div>
@@ -45,4 +46,5 @@ ImageCard.propTypes = {
   buttonText: PropTypes.string.isRequired,
   isUserCard: PropTypes.bool,
   isEngagementCard: PropTypes.bool,
+  handleButtonClick: PropTypes.func.isRequired,
 };
