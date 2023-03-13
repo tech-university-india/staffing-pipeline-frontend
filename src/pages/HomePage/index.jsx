919ddbf04bf8bf71d4700e4ec7f094a984bb3db1
chiranshu khandelwal <chiranshumain2gmail.com>
2023-03-13 12:49:40 +0530

    chore: add header to homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './HomePage.css';
 import Footer from '../../components/Footer';
+import { Header } from '../../components';
 const HomePage = () => {
   return (
     <div>
+      <Header hasNav={true} />
       HomePage
       <Footer />
     </div>
