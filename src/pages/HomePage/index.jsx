89c03c0d8290257d7df3c0bb38a7f4e1c37e9b3d
rishusingh022 <rishusingh022@gmail.com>
2023-03-14 04:21:42 +0530

    feat : add search component

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './HomePage.css';
 import Footer from '../../components/Footer';
 import { Header } from '../../components';
+import { Search } from '../../components';
 const HomePage = () => {
   return (
     <div>
       <Header hasNav={true} />
-      HomePage
+      <Search />
       <Footer />
     </div>
   );
