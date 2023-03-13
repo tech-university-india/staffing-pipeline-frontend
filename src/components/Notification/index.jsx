bf501e043e5fb4e2e625e286140aa60849d5618f
Promit-revar <promit.revar2211@gmail.com>
2023-03-13 22:56:11 +0530

    fix: code linting

--- src/components/Notification/index.jsx
+++ src/components/Notification/index.jsx
@@ -5,11 +5,15 @@ import propTypes from 'prop-types';
 
 export default function Notification({ message }) {
   const [show, setShow] = React.useState(true);
-  if(show){
+  if (show) {
     return (
       <div className={'notification-card'}>
-        <div className='message'><p>{message}</p></div>
-        <div className='close-button'onClick={()=>setShow(false)}>X</div>
+        <div className="message">
+          <p>{message}</p>
+        </div>
+        <div className="close-button" onClick={() => setShow(false)}>
+          X
+        </div>
       </div>
     );
   }
