f2b7ebcbcf23d79fdeb510b09a64ede0d584f982
Aganya Bajaj <aganyabajaj7@gmail.com>
2023-03-13 15:41:53 +0530

    fix: fixed McKinsey logo aspect ratio in the header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,10 @@ export default function Header({ hasNav }) {
   return (
     <div className="font-light h-[88px] text-white flex justify-center bg-deepBlue">
       <div className="h-full xl:w-[63vw] lg:[70vw] md:[80vw] flex box-border">
-        <img src="./assets/McK_Logo.png" className="h-[70px] self-end pl-0 lg:pr-[180px] sm:pr-[90px] pt-0 pb-[5px]" />
+        <img
+          src="./assets/McK_Logo.png"
+          className="h-[70px] w-[155px] aspect-auto self-end pl-0 lg:mr-[180px] sm:mr-[90px] pt-0 pb-[5px]"
+        />
         {hasNav && (
           <div className="grid grid-cols-3 gap-10 items-end">
             <button className="text-[white] hover:text-cyan hover:border-b-[5px] hover:border-b-cyan hover:border-solid pt-0 pb-[15px] hover:pb-2.5 px-[5px]">
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './HomePage.css';
-import { Header } from '../../components';
 import Footer from '../../components/footer';
 const HomePage = () => {
   return (
     <div>
-      <Header hasNav={true} />
       HomePage
       <Footer />
     </div>
