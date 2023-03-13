9d861ea1708e51e328c28f652ff1df13858d7bea
Promit-revar <promit.revar2211@gmail.com>
2023-03-13 22:55:30 +0530

    Feat: created error notification component

---
+++ src/components/Notification/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './notification.css';
+import propTypes from 'prop-types';
+
+export default function Notification({ message }) {
+  const [show, setShow] = React.useState(true);
+  if(show){
+    return (
+      <div className={'notification-card'}>
+        <div className='message'><p>{message}</p></div>
+        <div className='close-button'onClick={()=>setShow(false)}>X</div>
+      </div>
+    );
+  }
+}
+Notification.propTypes = {
+  message: propTypes.string,
+};
